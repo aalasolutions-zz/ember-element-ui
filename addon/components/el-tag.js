fc91ad37cd824aed76fe5440dc35cf18bf08269b
Aamir Mahmood <aamir@aalasolutions.com>
2018-11-12 11:16:13 +0500

    el-tag completed

---
+++ addon/components/el-tag.js
@@ -0,0 +1,51 @@
+import Component from '@ember/component';
+import layout from '../templates/components/el-tag';
+import {computed, get} from "@ember/object";
+
+export default Component.extend({
+  layout,
+  tagName: 'span',
+  classNames: ['el-tag'],
+
+  classNameBindings: ['getClassName',
+    'hit:is-hit',
+  ],
+
+
+  getClassName: computed('type', 'size', function () {
+
+    let classNames = '';
+
+    if (get(this, 'type')) {classNames += ` el-tag--${get(this, 'type')}`;}
+    if (get(this, 'size')) {classNames += ` el-tag--${get(this, 'size')}`;}
+
+    return classNames;
+  }),
+
+
+  closable: false,
+  type: '',
+  hit: false,
+  color: '',
+  size: '',
+
+  handleClose: null,
+
+
+  actions: {
+    handleClose() {
+      this.$().addClass('animated flipOutY');
+      this.$().one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+        () => {
+          if (this.get('close')) {
+            this.get('close')();
+          }
+        }
+      );
+
+
+    }
+  }
+
+
+});
