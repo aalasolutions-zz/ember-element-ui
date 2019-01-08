179fe88766d859ac06a0f2686ce3a4ce3f3f5c19
Aamir Mahmood <aamir@aalasolutions.com>
2019-01-08 15:51:44 +0500

    fixed issue in test - undefined var

--- tests/unit/utils/transition-test.js
+++ tests/unit/utils/transition-test.js
@@ -1,7 +1,7 @@
 import transition from 'dummy/utils/transition';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | transition', function(hooks) {
+module('Unit | Utility | transition', function() {
 
   // Replace this with your real tests.
   test('it works', function(assert) {
