b6124be8966a9d85fa4a0d8d682a59291f84d309
Aamir Mahmood <aamir@aalasolutions.com>
2019-05-22 14:40:17 +0500

    Highlight code

--- tests/dummy/app/routes/input.js
+++ tests/dummy/app/routes/input.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    didTransition() {
+      later(this, () => {
+        Prism.highlightAll();
+      }, 100);
+    }
+  }
 });
