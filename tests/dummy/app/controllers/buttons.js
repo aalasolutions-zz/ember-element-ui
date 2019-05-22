f98ef80c58314dedd0245615bc39b953c5817d33
Muhammad Ilyas <ilyas@aalasolutions.com>
2019-05-22 16:56:08 +0500

    button dummy doc style change

--- tests/dummy/app/controllers/buttons.js
+++ tests/dummy/app/controllers/buttons.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  tableAttributes: [
+    { attribute: 'size', desc: "button size", type: "medium / small / mini", val: '-', def: '-' },
+    { attribute: 'color', desc: "button type", type: "string", val: 'primary / success / warning / danger / info / text', def: '-' },
+    { attribute: 'plain', desc: "determine whether it's a plain button", type: "boolean", val: '-', def: 'false' },
+    { attribute: 'round', desc: "determine whether it's a round button", type: "boolean", val: '-', def: 'false' },
+    { attribute: 'circle', desc: "determine whether it's a circle button", type: "boolean", val: '-', def: 'false' },
+    { attribute: 'loading', desc: "determine whether it's loading	", type: "boolean", val: '-', def: 'false' },
+    { attribute: 'disabled', desc: "disable the button", type: "boolean", val: '-', def: 'false' },
+    { attribute: 'icon', desc: "icon", type: "string", val: 'icon-name', def: '-' },
+  ],
 });
