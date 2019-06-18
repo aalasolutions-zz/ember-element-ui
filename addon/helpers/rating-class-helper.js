48873ca5b747ebf700c93ee178e38ebc337991ad
Muhammad Ilyas <ilyas@aalasolutions.com>
2019-06-18 16:07:45 +0500

    el-rate disable void color fix

--- addon/helpers/rating-class-helper.js
+++ addon/helpers/rating-class-helper.js
@@ -4,17 +4,18 @@ import { htmlSafe } from '@ember/template';
 export default Helper.extend({
 
   compute(params) {
-    let item          = params[0];
-    let classes       = params[1];
-    let style         = params[2];
-    let disabled      = params[3];
-    let color         = params[4];
-    let lowThreshold  = params[5];
-    let highThreshold = params[6];
-    let max           = params[7];
-    let colors        = params[8];
-    let currentValue  = params[9];
-    let value         = params[10];
+    let item              = params[0];
+    let classes           = params[1];
+    let style             = params[2];
+    let disabled          = params[3];
+    let color             = params[4];
+    let lowThreshold      = params[5];
+    let highThreshold     = params[6];
+    let max               = params[7];
+    let colors            = params[8];
+    let currentValue      = params[9];
+    let value             = params[10];
+    let disabledVoidColor = params[11];
     // let color         = "rgb(247, 186, 42)";
 
     if (style) {
@@ -30,6 +31,8 @@ export default Helper.extend({
       }
       if (classes[item - 1] === "el-icon-star-on" && (!disabled || item < value)) {
         return htmlSafe(`color: ${color};`)
+      } else if (item > value && disabled) {
+         return htmlSafe(`color: ${disabledVoidColor};`)
       }
       return;
     }
