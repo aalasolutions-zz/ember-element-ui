cbd00e7cf58cd8e3b4d05dfe980ec29098ad5052
Aamir Mahmood <aamir@aalasolutions.com>
2019-11-12 14:27:43 +0500

    Unit tests updated

--- tests/integration/components/el-checkbox-button-test.js
+++ tests/integration/components/el-checkbox-button-test.js
@@ -5,29 +5,29 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | el-checkbox-button', function(hooks) {
   setupRenderingTest(hooks);
-  //
-  // test('it renders', async function(assert) {
-  //   // Set any properties with this.set('myProperty', 'value');
-  //   // Handle any actions with this.set('myAction', function(val) { ... });
-  //
-  //   await render(hbs`{{el-checkbox-button}}`);
-  //
-  //   assert.equal(this.element.textContent.trim(), '');
-  //
-  //   // Template block usage:
-  //   await render(hbs`
-  //     {{#el-checkbox-button}}
-  //       template block text
-  //     {{/el-checkbox-button}}
-  //   `);
-  //
-  //   assert.equal(this.element.textContent.trim(), 'template block text');
-  //
-  //
-  //   this.set('size', 'large');
-  //   await render(hbs`{{el-checkbox-button size=size}}`);
-  //
-  // });
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{el-checkbox-button}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#el-checkbox-button}}
+        template block text
+      {{/el-checkbox-button}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+
+
+    this.set('size', 'large');
+    await render(hbs`{{el-checkbox-button size=size}}`);
+
+  });
 
   test('parent', async function(assert){
 
