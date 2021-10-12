43e835ecfe2afd16ae13f4dd1f0457e5f54cb08f
shahroz <shahroz@aalasolutions.com>
2021-10-12 12:26:59 +0500

    upgraded to octane

--- addon/components/el-switch.js
+++ addon/components/el-switch.js
@@ -1,63 +1,110 @@
-import Component  from '@ember/component';
-import layout     from '../templates/components/el-switch';
-import {computed} from "@ember/object";
+import Component  from '@glimmer/component';
+import {action, computed} from "@ember/object";
 import {htmlSafe} from '@ember/template';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  layout,
-  classNames: ['el-switch'],
-  tagName   : 'label',
+export default class ElSwitchComponent extends Component {
 
-  id               : '',
-  name             : '',
-  disabled         : false,
-  width            : '',
-  activeIconClass  : '',
-  inactiveIconClass: '',
-  activeText       : '',
-  inactiveText     : '',
-  activeValue      : true,
-  inactiveValue    : false,
-  validateEvent    : true,
-  activeColor      : '',
-  inactiveColor    : '',
-  model            : false,
+  @tracked model;
 
-  isChecked: computed('model', function () {
-    return !!this.get('model');
-  }),
+  constructor(owner, args) {
+    super(owner, args);
+    this.model = this.args.model;
+  }
 
-  notChecked: computed('model', function () {
-    return !this.get('model');
-  }),
+  // disabled         : false,
+  @computed ("args.disabled")
+  get isDisabled() {
+    return this.args.disabled || false;
+  }
+  // activeIconClass  : '',
+  @computed ("args.activeIconClass")
+  get activeIconClass() {
+    return this.args.activeIconClass || '';
+  }
+  // inactiveIconClass: '',
+  @computed ("args.inactiveIconClass")
+  get inactiveIconClass() {
+    return this.args.inactiveIconClass || '';
+  }
+  // activeText       : '',
+  @computed ("args.activeText")
+  get activeText() {
+    return this.args.activeText || '';
+  }
+  // inactiveText     : '',
+  @computed ("args.inactiveText")
+  get inactiveText() {
+    return this.args.inactiveText || '';
+  }
+  // activeValue      : true,
+  @computed ("args.activeValue")
+  get activeValue() {
+    return this.args.activeValue || true;
+  }
+  // inactiveValue    : false,
+  @computed ("args.inactiveValue")
+  get inactiveValue() {
+    return this.args.inactiveValue || false;
+  }
+  // validateEvent    : true,
+  @computed ("args.validateEvent")
+  get validateEvent() {
+    return this.args.validateEvent || true;
+  }
+  // activeColor      : '',
+  @computed ("args.activeColor")
+  get activeColor() {
+    return this.args.activeColor || '';
+  }
+  // inactiveColor    : '',
+  @computed ("args.inactiveColor")
+  get inactiveColor() {
+    return this.args.inactiveColor || '';
+  }
 
-  isInactive: computed.or('inactiveIconClass', 'inactiveText'),
+  get isChecked() {
+    return !!this.model;
+  }
 
-  notInactiveIconClass: computed('inactiveIconClass', function () {
-    return !this.get('inactiveIconClass');
-  }),
+  get notChecked() {
+    return !this.model;
+  }
 
-  isInactiveText: computed.and('notInactiveIconClass', 'inactiveText'),
+  get isInactive() {
+    return this.inactiveIconClass || this.inactiveText;
+  }
 
+  get notInactiveIconClass() {
+    return !this.inactiveIconClass;
+  }
 
-  isActive: computed.or('activeIconClass', 'activeText'),
+  get isInactiveText() {
+    return this.notInactiveIconClass && this.inactiveText;
+  }
 
-  notActiveIconClass: computed('isActive', function () {
-    return !this.get('activeIconClass');
-  }),
+  get isActive() {
+    return this.activeIconClass && this.activeText;
+  }
 
-  isActiveText: computed.and('notActiveIconClass', 'activeText'),
+  get notActiveIconClass() {
+    return !this.activeIconClass;
+  }
 
+  get isActiveText() {
+    return this.notActiveIconClass && this.activeText;
+  }
 
-  spanStyle: computed("isChecked", "inactiveColor", "activeColor", 'width', function () {
-    let color = this.get("model") ? this.get("activeColor") : this.get("inactiveColor");
-    let width = this.get("width");
-
-    if (width === '') {
-      width = 40;
-    }
-
+  get spanStyle() {
+    const color = this.model ? this.activeColor : this.inactiveColor;
+    const width = this.width || 40;
     return htmlSafe(`background: ${color}; border-color: ${color}; width: ${width}px;`);
-  }),
+  }
 
-});
+  @action 
+  handleAction () {
+    if(!this.isDisabled)
+    this.model  = !this.model;
+  }
+
+}
